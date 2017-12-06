faf481c30d17753d5e76b9e6173ed9066b462775
ash.fayzrahmanov <ash.fayzrahmanov@tensor.ru>
2017-12-06 16:04:42 +0300

    исполняем задания по попавшим шагам. веперед, назад, прыхок.

--- main.js
+++ main.js
@@ -9,12 +9,13 @@ new Vue({
          if (where > 48 || where < 1) return;
          var pathEl = undefined;
          var step = (obj === "player") ? this.stepP : this.stepC;
+         step = (step > where) ? step = step - 1 : step = step + 1;
          if (jamp) {
             pathEl = this.pathArr[where - 1];
             step = where
          }
 
-         step = (step > where) ? step = step - 1 : step = step + 1;
+
          pathEl = (jamp) ? pathEl : this.pathArr[step - 1];
          (obj === "player") ? this.stepP = step : this.stepC = step;
 
@@ -31,12 +32,40 @@ new Vue({
                this.autoStep(obj, where, jamp, transition);
             }.bind(this), 500);
          } else {
-            console.log(" ==transition= ", transition)
-            if (transition) {
-               this.onForAnotherPlayer(obj == "player" ? "computer" : "player");
-            } else{
-               this.message = "Ваш ход"
+            // либо передаем ход, либо обрабатываем сюрпризы
+
+            var point = this.pathArr[where - 1].point;
+            if(point){
+               this.message = this.surprise[point];
             }
+
+            setTimeout(function(){this.middleware(obj, where, transition)}.bind(this), 500);
+         }
+
+      },
+      middleware: function(obj, where, transition){
+         var point = this.pathArr[where - 1].point;
+
+
+         switch (point) {
+            case "forward":
+               return this.autoStep(obj, where + 1, null, transition);
+            case "backward":
+               return  this.autoStep(obj, where - 1, null, transition);
+            case "wait":
+               console.log(" ==ДУМАТЬ -- тут либо задать ребенку математическую задачу, чтобы выйти из ловушки, либо по условию= ");
+               return;
+            case "teleport":
+               var where = +this.pathArr[where - 1].jump;
+               return this.autoStep(obj, where, true, transition);
+            default:
+
+         }
+
+         if (transition) {
+            this.onForAnotherPlayer(obj == "player" ? "computer" : "player");
+         } else{
+            setTimeout(function(){this.message = "Ваш ход"}.bind(this),1000);
          }
 
       },
@@ -76,6 +105,9 @@ new Vue({
             setTimeout(function () {
                var random = Math.round(Math.random() * 5 + 1);
                this.defaultBone = random;
+
+               // this.defaultBone = 9;
+
                this.onBone(obj);
                this.onStepAfterBone(obj);
             }.bind(this), 500)
@@ -91,8 +123,10 @@ new Vue({
       onForAnotherPlayer: function (obj) {
          // переход другому игроку
          if (obj == "computer") {
-            this.message = "Ход комьютера"
-            this.onBone(obj)
+            setTimeout(function () {
+               this.message = "Ход комьютера";
+               this.onBone(obj)
+            }.bind(this), 1000);
          }
       }
 
@@ -127,47 +161,47 @@ new Vue({
          {id: 1, x: 197, y: 400},
          {id: 2, x: 273, y: 385},
          {id: 3, x: 343, y: 399},
-         {id: 4, x: 383, y: 456},
+         {id: 4, x: 383, y: 456, point:"forward"},
          {id: 5, x: 437, y: 503},
          {id: 6, x: 505, y: 481},
          {id: 7, x: 572, y: 509},
-         {id: 8, x: 581, y: 580},
-         {id: 9, x: 623, y: 651},
+         {id: 8, x: 581, y: 580, point:"backward"},
+         {id: 9, x: 623, y: 651, point:"wait"},
          {id: 10, x: 701, y: 679},
          {id: 11, x: 797, y: 671},
          {id: 12, x: 875, y: 642},
-         {id: 13, x: 929, y: 590},
+         {id: 13, x: 929, y: 590, point:"teleport", jump:"18"},
          {id: 14, x: 970, y: 535},
-         {id: 15, x: 975, y: 476},
+         {id: 15, x: 975, y: 476, point:"forward"},
          {id: 16, x: 944, y: 412},
          {id: 17, x: 878, y: 389},
          {id: 18, x: 808, y: 387},
          {id: 19, x: 772, y: 441},
-         {id: 20, x: 742, y: 505},
+         {id: 20, x: 742, y: 505, point:"wait"},
          {id: 21, x: 663, y: 494},
-         {id: 22, x: 633, y: 418},
+         {id: 22, x: 633, y: 418, point:"backward"},
          {id: 23, x: 702, y: 374},
          {id: 24, x: 740, y: 311},
-         {id: 25, x: 654, y: 281},
-         {id: 26, x: 584, y: 313},
+         {id: 25, x: 654, y: 281, point:"wait"},
+         {id: 26, x: 584, y: 313, point:"teleport", jump:"33"},
          {id: 27, x: 510, y: 309},
          {id: 28, x: 445, y: 348},
-         {id: 29, x: 377, y: 325},
+         {id: 29, x: 377, y: 325, point:"forward"},
          {id: 30, x: 338, y: 268},
-         {id: 31, x: 325, y: 205},
-         {id: 32, x: 374, y: 157},
+         {id: 31, x: 325, y: 205, point:"backward"},
+         {id: 32, x: 374, y: 157, point:"wait"},
          {id: 33, x: 449, y: 151},
          {id: 34, x: 522, y: 163},
          {id: 35, x: 583, y: 129},
-         {id: 36, x: 650, y: 134},
+         {id: 36, x: 650, y: 134, point:"backward"},
          {id: 37, x: 726, y: 124},
-         {id: 38, x: 799, y: 134},
+         {id: 38, x: 799, y: 134, point:"forward"},
          {id: 39, x: 870, y: 110},
-         {id: 40, x: 924, y: 137},
+         {id: 40, x: 924, y: 137, point:"teleport", jump:"45"},
          {id: 41, x: 876, y: 174},
          {id: 42, x: 835, y: 225},
-         {id: 43, x: 854, y: 285},
-         {id: 44, x: 913, y: 326},
+         {id: 43, x: 854, y: 285, point:"wait"},
+         {id: 44, x: 913, y: 326, point:"backward"},
          {id: 45, x: 989, y: 354},
          {id: 46, x: 1049, y: 402},
          {id: 47, x: 1123, y: 434},
@@ -176,5 +210,11 @@ new Vue({
       myTimer: null,
       message: "Ваш ход. Нажмите кость.",
       questionMind: false,
+      surprise: {
+         wait: " Ловушка. Тролль ходит только когда на кубике выпадает больше 3-x",
+         forward: " Повезло! Тролль может сделать дополнительный ход",
+         backward: " Троллю нужно сделать шаг назад",
+         teleport: " Тролль может срезать путь по белым стрелочкам"
+      }
    },
 });
